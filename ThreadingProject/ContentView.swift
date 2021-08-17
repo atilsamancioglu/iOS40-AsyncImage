1312b614fca02c235d9821ac5e5616cd42929a30
Atıl Samancıoğlu <samancioglu.atil@gmail.com>
2021-08-17 14:46:31 +0300

    Initial Commit

---
+++ ThreadingProject/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ThreadingProject
+//
+//  Created by Atil Samancioglu on 17.08.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ThreadingProject/ThreadingProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  ThreadingProjectApp.swift
+//  ThreadingProject
+//
+//  Created by Atil Samancioglu on 17.08.2021.
+//
+
+import SwiftUI
+
+@main
+struct ThreadingProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
